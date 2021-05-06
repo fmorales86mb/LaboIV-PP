84b0573558bfd953768034b66d7ead4eaab74cb6
fede <win-notebook>
2021-05-06 15:12:57 -0300

    Conexión a Firebase + generic.service

---
+++ src/app/services/generic.service.ts
@@ -0,0 +1,28 @@
+import { AngularFirestore, AngularFirestoreCollection } from "@angular/fire/firestore";
+import { Observable } from "rxjs";
+
+export class Generic<T>{
+    protected itemsCollection: AngularFirestoreCollection<T>;
+    items: Observable<T[]>;    
+
+    constructor(private afs: AngularFirestore) {
+      
+    }
+
+    setCollection(collName:string){
+      this.itemsCollection = this.afs.collection<T>(collName);
+      this.items = this.itemsCollection.valueChanges();
+    }
+  
+    addItem(item: T) {
+      return this.itemsCollection.add(Object.assign({}, item));    
+    }
+
+    setItemWithId(item: T, id:string) {
+      return this.itemsCollection.doc(id).set(Object.assign({}, item));    
+    }
+    
+    getItem(id:string){
+      return this.itemsCollection.doc(id).get();
+    }
+}
